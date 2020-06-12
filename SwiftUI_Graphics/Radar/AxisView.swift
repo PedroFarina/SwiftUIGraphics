69940e06b5cb046312f198d52e45d73da19a7121
Paula Leite <paula.t.leite@icloud.com>
2020-06-12 14:00:53 -0300

    findPoint structured

--- SwiftUI_Graphics/Radar/AxisView.swift
+++ SwiftUI_Graphics/Radar/AxisView.swift
@@ -18,6 +18,8 @@ struct AxisView: View {
     
     var color: Color
     var components: [AxisComponent]
+    @State var center: CGPoint = .zero
+    let axisRadius: Double = 150
     
     var degreeInterval: Double {
         return (Double.pi * 2) / Double(components.count)
@@ -26,6 +28,19 @@ struct AxisView: View {
     init(color: Color = .random(), _ components: AxisComponent...) {
         self.components = components
         self.color = color
+    }
+    
+    func findPoint(_ value: Double, in axis: AxisComponent) -> CGPoint? {
+        if let index = components.firstIndex(of: axis) {
+            
+            let radius = (value / axis.scale.upperBound - axis.scale.lowerBound) * axisRadius
+            let radians = (Double(index) + 1) * degreeInterval
+            
+            return center.findPointIn(radius: radius, radians: radians)
+        }
+        
+        return nil
+        
     }
     
     var body: some View {
@@ -37,13 +52,13 @@ struct AxisView: View {
                 ZStack {
                     
                     LineView(color: self.color) { () -> [CGPoint] in
-                        let center = geoProxy.center()
+                        self.center = geoProxy.center()
 
                         let interval = self.degreeInterval * Double(index + 1)
                 
-                        let nextPoint = center.findPointIn(radius: 150, radians: interval)
+                        let nextPoint = self.center.findPointIn(radius: self.axisRadius, radians: interval)
                         
-                        return [center, nextPoint]
+                        return [self.center, nextPoint]
                     }
 
                     Text("\(self.components[index].subtitle)")
